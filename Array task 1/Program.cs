5e5304c5fdf28cfe1f4f06c741b7638f688f3e83
Omar Farid <omarfareed242@gmail.com>
2023-02-23 22:20:06 +0200

    Add project files.

---
+++ Array task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int inpsize = int.Parse(Console.ReadLine()), odd, even;
+even = 0; odd = 0;
+int[] array = new int[inpsize];
+
+for (int i = 0; i < inpsize; i++) {
+    Console.WriteLine("Please choose a value for index " + i);
+    array[i] = int.Parse(Console.ReadLine());    
+}
+
+for (int i = 0; i < inpsize; i++) {
+
+    if (i % 2 == 0) { even++;}
+    else { odd++; }
+};
+
+for (int i = 0; i < inpsize; i++)
+{
+
+    if (i % 2 == 0) { Console.WriteLine("An even number is:" + "{" + i +"}"); }
+    else { Console.WriteLine("An odd number is:" + "{" + i + "}"); }
+};
+
+
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("There are: " + even + " even elements");
+Console.WriteLine("There are: " + odd + " odd elements");
+Console.ReadKey();
+
+
